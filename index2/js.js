8ea087d7a3ff0cdf4b2e949fe4a10c5c57218f4a
NikitaMinz <115998002+NikitaMinz@users.noreply.github.com>
2023-06-22 09:02:59 +0500

    Add files via upload

---
+++ index2/js.js
@@ -0,0 +1,53 @@
+let batton = document.querySelector('.btn1') 
+let batton2 = document.querySelector('.btn2') 
+let batton3 = document.querySelector('.btn3') 
+let batton4 = document.querySelector('.btn4') 
+let batton5 = document.querySelector('.btn5') 
+let batton6 = document.querySelector('.btn6') 
+let batton7 = document.querySelector('.btn7') 
+let batton8 = document.querySelector('.btn8') 
+let batton9 = document.querySelector('.btn9') 
+let batton10 = document.querySelector('.btn10') 
+
+batton.addEventListener('click', function(){
+    window.location.href = 'https://investors.delltechnologies.com/'
+})
+batton2.addEventListener('click', function(){
+    window.location.href = 'https://newsroom.accenture.com/news/accenture-to-discontinue-business-in-russia.htm'
+})
+batton3.addEventListener('click', function(){
+    window.location.href = 'https://ir.synnex.com/ir-home/default.aspx'
+})
+batton4.addEventListener('click', function(){
+    window.location.href = 'https://www.ibm.com/about/russia/'
+})
+batton5.addEventListener('click', function(){
+    window.location.href = 'https://www.hpe.com/us/en/home.html'
+})
+batton6.addEventListener('click', function(){
+    window.location.href = 'https://www.cisco.com/?ysclid=lj40ur1vla513014640'
+})
+batton7.addEventListener('click', function(){
+    window.location.href = 'https://www.oracle.com/'
+})
+batton8.addEventListener('click', function(){
+    window.location.href = 'https://www.sap.com/cis/index.html?url_id=auto_hp_redirect_cis'
+})
+batton9.addEventListener('click', function(){
+    window.location.href = 'https://www.fujitsu.com/global/'
+})
+batton10.addEventListener('click', function(){
+    window.location.href = 'https://www.nec.com/'
+})
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index3/js.js
@@ -0,0 +1,30 @@
+let batton = document.querySelector('.btn1') 
+let batton2 = document.querySelector('.btn2') 
+let batton3 = document.querySelector('.btn3') 
+let batton4 = document.querySelector('.btn4') 
+let batton5 = document.querySelector('.btn5') 
+let batton6 = document.querySelector('.btn6') 
+let batton7 = document.querySelector('.btn7') 
+
+
+batton.addEventListener('click', function(){
+    window.location.href = 'https://onecomp.ru/linux-sozdanie-arhiva-s-parolem/?ysclid=lj40xerfix346715628'
+})
+batton2.addEventListener('click', function(){
+    window.location.href = 'https://habr.com/ru/companies/muk/articles/251959/'
+})
+batton3.addEventListener('click', function(){
+    window.location.href = 'https://klimov.com/news/article/cisco-predstavlyaet-resheniya-novogo-pokoleniya-rasshiryayushie-vozmozhnosti-bezopasnosti-i-proizvoditelnosti-s-pomoshyu-generativnogo-iskusstvennogo-intellekta/?ysclid=lj416hgffm92966669'
+})
+batton4.addEventListener('click', function(){
+    window.location.href = 'https://ru-cisco.com/news-2023-06-07/?ysclid=lj416wo8wi955679161'
+})
+batton5.addEventListener('click', function(){
+    window.location.href = 'https://habr.com/ru/news/739062/'
+})
+batton6.addEventListener('click', function(){
+    window.location.href = 'https://wylsa.com/chto-pokazali-na-prezentaczii-igr-xbox-bethesda/?ysclid=lj417yi96z433907289'
+})
+batton7.addEventListener('click', function(){
+    window.location.href = 'https://quote.rbc.ru/news/article/6475c82d9a7947eb695011c7?ysclid=lj418gwg7o803411955'
+})
